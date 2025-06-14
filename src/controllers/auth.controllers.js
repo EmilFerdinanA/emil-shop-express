f6f242fef5f424773ad1df2573ce44d3857dbad8
EmilFerdinanA <emilakbar55@gmail.com>
2025-06-14 17:30:53 +0700

    error middleware and hashed password

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,39 +1,38 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import Role from "../models/role.model.js";
 
-const signUp = async (req, res) => {
+const signUp = async (req, res, next) => {
   const session = await mongoose.startSession();
   session.startTransaction();
 
   try {
     const { username, email, password, role } = req.body;
 
-    // Cek apakah username sudah ada
     const existingUser = await User.findOne({ username }).session(session);
-    if (existingUser) {
-      await session.abortTransaction();
-      session.endSession();
-      return res
-        .status(400)
-        .json({ success: false, message: "Username already exists" });
+    const existingEmail = await User.findOne({ email }).session(session);
+    if (existingUser || existingEmail) {
+      const error = new Error("Username or Email already exists");
+      error.statusCode = 409;
+      throw error;
     }
 
-    // Cek atau buat role jika belum ada
     let existingRole = await Role.findOne({ name: role }).session(session);
     if (!existingRole) {
-      const newRoles = await Role.create([{ name: role }], { session });
-      existingRole = newRoles[0]; // ambil object role yang dibuat
+      [existingRole] = await Role.create([{ name: role }], { session });
     }
 
-    // Buat user baru
-    const newUsers = await User.create(
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [newUser] = await User.create(
       [
         {
           username,
           email,
-          password,
+          password: hashedPassword,
           role: existingRole.name,
+          active: false,
         },
       ],
       { session }
@@ -45,17 +44,12 @@ const signUp = async (req, res) => {
     return res.status(201).json({
       success: true,
       message: "User created successfully",
-      data: newUsers[0],
+      data: newUser,
     });
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
-
-    return res.status(500).json({
-      success: false,
-      message: "Internal server error",
-      error: error.message,
-    });
+    next(error);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import { PORT } from "./config/env.js";
 import connectDB from "./config/db.js";
 
 import authRouter from "./routes/auth.route.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   await connectDB();
